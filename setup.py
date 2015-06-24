b8956dcb8711995d21e47495a088ff9eb8719df0
oxpeter <oxpeter+git@gmail.com>
2015-06-24 15:54:24 -0400

    add setup.py to allow python install of files

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ortho',
+      version='0.5',
+      description='miscellany of python packages for all and sundry analyses',
+      author='Peter Oxley',
+      author_email='oxpeter+git@gmail.com',
+      url='https://oxpeter@bitbucket.org/oxpeter/ortho.git',
+      py_modules=[  'blastfaster', 'brain_machine', 'degrees',
+                    'kegg', 'longestORF',
+                    'plate_analysis', 'textmod',
+                  ],
+      requires=[ 'argparse' ]
+     )
